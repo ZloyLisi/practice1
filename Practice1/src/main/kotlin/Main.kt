98d644a30b5d723a5e786945909236c906dd6374
ZloyLisi <github.2005.18@gmail.com>
2023-09-06 00:00:34 +0300

    first commit

---
+++ Practice1/src/main/kotlin/Main.kt
@@ -0,0 +1,59 @@
+fun main(args: Array<String>) {
+
+    //задание 1
+    val exercises = 13
+    var exercisesSolved = 0
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 2
+    val myAge = 18
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 3
+    val x = 30.0
+    var arithmeticMeanOfAge = 18.0
+    arithmeticMeanOfAge = (arithmeticMeanOfAge + x)/2
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 4
+    val testNumber = 19
+    val evenOdd = testNumber % 2
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 5
+    var answer = (((0 + 1)  + 10) * 10) shr 3
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 6
+    var age = 16
+    print(age)
+    age = 30
+    print(age)
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 7
+    val answer1: Int = (46*100)+10
+    val answer2: Int = (46*100)+(10*100)
+    val answer3: Int = (46*100)-(10/10)
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 8
+    (5 * 3) - ((4/2)*2)
+
+    exercisesSolved =+ exercisesSolved + 1
+
+    //задание 9
+    val a = 10.0
+    val b = 16.0
+    val average = (a+b)/2
+
+    exercisesSolved =+ exercisesSolved + 1
+
+}
